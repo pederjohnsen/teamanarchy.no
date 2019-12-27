85d5883668878ee3ec5ebd68a5bdd5ab638a5bfb
Peder Johnsen <me@pederjohnsen.com>
2019-12-27 15:46:39 +0100

    backup of old site

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Team Anarchy</title>
+    <meta name="description" content="Team Anarchy.">
+    <meta name="author" content="Peder Johnsen">
+
+    <!-- Le styles -->
+    <link href="/media/css/prettify.css" type="text/css" rel="stylesheet">
+    <link href="/media/css/bootstrap.css" type="text/css" rel="stylesheet">
+    <link href="/media/css/theme.css" type="text/css" rel="stylesheet">
+    <style type="text/css">
+      body {
+            padding-top: 20px;
+    <!--    padding-top: 60px; -->
+      }
+    </style>
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le javascript -->
+    <script type="text/javascript" src="/media/js/jquery-1.5.1.min.js"></script>
+    <script type="text/javascript" src="/media/js/prettify.js"></script>
+    <script type="text/javascript" src="/media/js/bootstrap-twipsy.js"></script>
+    <script type="text/javascript" src="/media/js/bootstrap-popover.js"></script>
+    <script type="text/javascript" src="/media/js/bootstrap-buttons.js"></script>
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="/imgages/favicon.ico">
+  </head>
+  <body>
+
+    <div class="container">
+      <section>
+        <div class="row">
+          <div class="span16" style="text-align:center;margin-bottom:10px;">
+            <a href="http://www.anarchy.bloodbowlleague.com/"><img src="media/images/logo.png" alt="logo.png" width="519" height="106"></a>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class="row">
+          <div class="span16" style="text-align:center;">
+            <p><a id="facebook" href="https://www.facebook.com/groups/441495535894617"><img src="images/team-anarchy-facebook.png?1" alt="Team Anarchy Facebook" width="516"></a></p>
+            <p><a id="belugacampaign" href="https://www.dropbox.com/sh/zpwkajnlvv1ar6p/AABZur6vJE2oDM4d8urrZNM1a?dl=0" target="_blank"><img src="images/belugacampaign.jpg" alt="The Beluga Campaign" width="516"></a></p>
+            <p><a id="ttb-intervju" href="images/ttb_intervju.png" target="_blank"><img src="images/ttb_intervju_thumb.png" alt="TTB Intervju" width="720"></a></p>
+            <div style="width: 516px;margin:10px auto;">
+              <a href="https://www.zoho.com/mail/login.html" target="_blank" class="btn btn-default btn-block"><span class="glyphicon glyphicon-inbox"></span> E-post innlogging</a>
+            </div>
+            <p><img src="images/team-anarchy-forum.png?1" alt="Team Anarchy Forum" height="258"></p>
+            <p><a id="keffokup18" href="http://www.anarchy.bloodbowlleague.com/default.asp?p=sr&s=32" target="_blank"><img src="images/daKeffoKup18.jpg" alt="Keffo Kup 18"></a></p>
+            <p><a id="keffokup17" href="http://www.anarchy.bloodbowlleague.com/default.asp?p=sr&s=28" target="_blank"><img src="images/da_keffo_kup_17.png" alt="Keffo Kup 17"></a></p>
+            <p><a id="plakat4" href="https://www.facebook.com/groups/441495535894617/search/?query=%C3%A5pen%20dag&__mref=message_bubble"><img src="images/plakat-4.jpg" alt="&Aring;pen dag l&oslash;rdag 9. april" width="664"></a></p>
+            <p><a id="plakat3" href="https://www.facebook.com/groups/441495535894617/search/?query=%C3%A5pen%20dag&__mref=message_bubble"><img src="images/plakat-3.jpg" alt="Plakat" width="664"></a></p>
+            <p><a id="plakat2" href="https://www.facebook.com/events/836640579729194"><img src="images/northernbattlefieldsii.png" alt="Plakat" width="664"></a></p>
+            <p><a id="plakat" href="https://www.facebook.com/groups/441495535894617/search/?query=%C3%A5pen%20dag&__mref=message_bubble"><img src="images/plakat.jpg" alt="Plakat" width="664"></a></p>
+          </div>
+        </div>
+      </section>
+
+      <script>
+      $(document).ready(function() {
+        $("a[rel=twipsy]").twipsy({
+          placement: "below"
+        });
+      })
+      </script>
+
+    </div> <!-- /container -->
+
+  </body>
+</html>
